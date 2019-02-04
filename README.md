a125db3a8fee1d83941f119ba6a0618c2617450c
cbonoz <chrisdistrict@gmail.com>
2019-02-03 23:11:21 -0500

    update sample

--- README.md
+++ README.md
@@ -16,9 +16,29 @@ mesg-core service deploy https://github.com/cbonoz/mesg19
 // TODO
 
 ### Tasks
-// TODO
+`See mesg.yml`
 
 
 ## Example Application
 * Issue and receive a payment via https://github.com/mesg-foundation/service-stripe
 * Upload a receipt to Amazon S3 via MESG3 (this repo).
+
+### How to setup example
+* Deploy and start the services
+<pre>
+    mesg-core start
+    mesg-core service deploy https://github.com/cbonoz/mesg19
+    mesg-core service deploy https://github.com/mesg-foundation/service-stripe
+    mesg-core service start https://github.com/mesg-foundation/service-stripe
+    mesg-core service start https://github.com/cbonoz/mesg19
+<pre>
+* Define the following environment TEST (or production) credentials:
+<pre>
+    AWS_ACCESS_TEST=<YOUR_AWS_ACCESS_KEY_HERE>
+    AWS_SECRET_TEST=<YOUR_AWS_SECRET_KEY_HERE>
+    STRIPE_API_KEY=<YOUR_STRIPE_API_KEY>
+</pre>
+* Call the stripe test workflow.
+<pre>
+    node sample_app/stripe.js
+</pre>
--- sample_app/stripe.js
+++ sample_app/stripe.js
@@ -3,24 +3,49 @@ const fs = require('fs')
 
 const accessKeyId = process.env.AWS_ACCESS_TEST
 const secretAccessKey = process.env.AWS_SECRET_TEST
+const stripeApiKey = process.env.STRIPE_API_KEY
+
+const stripe = require("stripe")(stripeApiKey);
+
+// User defined params
 const region = 'us-east-1'
+const receiptBucket = 'buildsage' // Your custom bucket.
 
-const body = {
-    accessKeyId,
-    secretAccessKey,
-    region,
-}
+const PORT = 3004;
 
-fetch(`http://localhost:${PORT}/${resource}`, {
-    method: 'post',
-    body:    JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
-})
-.then(res => {
-    if (resource === 'download') {
-        return res.text()
+(async () => {
+    try {
+        const stripeResponse = await stripe.charges.create({
+            amount: 2000,
+            currency: "usd",
+            source: "tok_amex", // obtained with Stripe.js
+            metadata: { 'order_id': '6735' }
+        });
+        const receiptUrl = stripeResponse.receipt_url
+        const receiptId = stripeResponse.id
+        // console.log('response', stripeResponse)
+
+        const receiptResponse = await fetch(receiptUrl)
+        const receiptData = await receiptResponse.text()
+        // console.log('receiptData', receiptData)
+
+        const body = {
+            accessKeyId,
+            secretAccessKey,
+            region,
+            bucket: receiptBucket, // Your custom bucket name
+            key: receiptId + "-stripe.html",
+            data: receiptData
+        }
+        
+        const uploadResponse = await fetch(`http://localhost:${PORT}/upload`, {
+            method: 'post',
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' },
+        })
+        const result = await uploadResponse.text()
+        console.log('upload result', result, 'file', body.key)
+    } catch (e) {
+        console.error('error', e)
     }
-    return res.json()
-})
-.then(json => console.log(json))
-.catch(console.error)
+})()
