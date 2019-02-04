b157310583fe8f17a65831b9c6e30e3f7812661b
cbonoz <chrisdistrict@gmail.com>
2019-02-03 22:29:02 -0500

    update test

--- sample_app/test.js
+++ sample_app/test.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch')
+const fs = require('fs')
 
 // https://pay.stripe.com/receipts/acct_1032D82eZvKYlo2C/ch_1DyHJ42eZvKYlo2CLMjQjvbf/rcpt_ER9ceIpeeJP3NvWxz8Aa5qY15N1v6H2
 
@@ -12,17 +13,39 @@ if (!resource) {
     return
 }
 
-
-
 const body = {
     accessKeyId,
     secretAccessKey,
     region,
-    bucket: 'buildsage',
-    key: 'service-worker.js'
 }
 
-const mesg = require('mesg-js').application()
+if (resource === 'upload') {
+    const bucket = process.argv[3]
+    const fileName = process.argv[4]
+    if (!bucket || !fileName) {
+        console.log('USAGE:  node test.js upload <bucket> <fileName>')
+        return 
+    }
+    body.bucket = bucket
+    body.key = fileName
+    body.data = fs.readFileSync(fileName)
+} else if (resource === 'download') {
+    const bucket = process.argv[3]
+    const fileName = process.argv[4]
+    if (!bucket || !fileName) {
+        console.log('USAGE:  node test.js download <bucket> <fileName>')
+        return 
+    }
+    body.bucket = bucket
+    body.key = fileName
+} else if (resource === 'objects') {
+    const bucket = process.argv[3] 
+    if (!bucket) {
+        console.log('USAGE:  node test.js objects <bucket>')
+        return 
+    }
+    body.bucket = bucket
+}
 
 const PORT = 3003
 
@@ -31,7 +54,12 @@ fetch(`http://localhost:${PORT}/${resource}`, {
         body:    JSON.stringify(body),
         headers: { 'Content-Type': 'application/json' },
     })
-    .then(res => res.json())
+    .then(res => {
+        if (resource === 'download') {
+            return res.text()
+        }
+        return res.json()
+    })
     .then(json => console.log(json))
     .catch(console.error)
 
