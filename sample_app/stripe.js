c5da1bd57a16a587070aee30809612c558dc776c
cbonoz <chrisdistrict@gmail.com>
2019-02-04 19:55:18 -0500

    update readme and sample

--- sample_app/stripe.js
+++ sample_app/stripe.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch')
-const fs = require('fs')
 
 const accessKeyId = process.env.AWS_ACCESS_TEST
 const secretAccessKey = process.env.AWS_SECRET_TEST
@@ -8,15 +7,16 @@ const stripeApiKey = process.env.STRIPE_API_KEY
 const stripe = require("stripe")(stripeApiKey);
 
 // User defined params.
-const region = 'us-east-1'
+const region = 'us-east-1' // specify region
 const receiptBucket = 'buildsage' // Your custom bucket.
 
 const PORT = 3004;
 
 (async () => {
     try {
+        // 1. Create the stripe payment
         const stripeResponse = await stripe.charges.create({
-            amount: 2000,
+            amount: 100,
             currency: "usd",
             source: "tok_amex", // obtained with Stripe.js
             metadata: { 'order_id': '6735' }
@@ -24,6 +24,7 @@ const PORT = 3004;
         const receiptUrl = stripeResponse.receipt_url
         const receiptId = stripeResponse.id
 
+        // 2. Download the receipt (as bytes) for the transaction from the stripe response.
         const receiptResponse = await fetch(receiptUrl)
         const receiptData = await receiptResponse.text()
 
@@ -36,11 +37,14 @@ const PORT = 3004;
             data: receiptData
         }
         
+        // 3. Upload the receipt bytes to s3.
         const uploadResponse = await fetch(`http://localhost:${PORT}/upload`, {
             method: 'post',
             body: JSON.stringify(body),
             headers: { 'Content-Type': 'application/json' },
         })
+
+        // Print the response result.
         const result = await uploadResponse.text()
         console.log('upload result', result, 'file', body.key)
     } catch (e) {
